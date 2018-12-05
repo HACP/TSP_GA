1550adbf19d4590c458f29e5288c41aeec1249ca
Hugo <hugo.a.contreras.p@gmail.com>
2018-12-05 16:26:50 -0500

    Adding util library

---
+++ tsp.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+
+import tsp_utils
+
+np.random.seed(2018)
+
+data = pd.read_csv("../data/all/cities.csv")
+
+data['prime'] = data['CityId'].isin(tsp_utils.primesfrom2to(data.shape[0]))
+
+print data.head()
